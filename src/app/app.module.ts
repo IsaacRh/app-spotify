34f0f99086eda7040ca24c3b0a3c6a1f98c79049
IsaacRh <isaac.roj_12@hotmail.com>
2020-11-30 16:13:42 -0600

    [ADD] --create main components and add Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { NabvarComponent } from './components/shared/nabvar/nabvar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistComponent,
+    NabvarComponent
   ],
   imports: [
     BrowserModule
