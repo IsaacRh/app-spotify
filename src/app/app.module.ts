09a12d97c94e8abcaadf949f8c1242483286f3e0
IsaacRh <isaac.roj_12@hotmail.com>
2020-11-30 19:57:05 -0600

    [CREATE] -- example fot http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from "@angular/router";
+
+// import httpclient
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { NabvarComponent } from './components/shared/nabvar/nabvar.component';
+
+//import routes
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -16,7 +24,9 @@ import { NabvarComponent } from './components/shared/nabvar/nabvar.component';
     NabvarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES, {useHash : true}) //import routes from app.routes.ts
   ],
   providers: [],
   bootstrap: [AppComponent]
